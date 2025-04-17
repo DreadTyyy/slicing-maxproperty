51d3d2c4c865bb2050826f0d5c7c9ba0a3e6d3ed
adib <adibhaidarzaky@gmail.com>
2025-04-18 05:36:56 +0700

    setting font

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import Affiliate from '@/pages/Affiliate'
 import BuyNFT from '@/pages/BuyNFT';
 import Earning from '@/pages/Earning';
 import Rewards from '@/pages/Rewards';
-import '@/style/style.css';
+import Collection from '@/pages/Collection';
 
 function App() {
 
   return (
-    <Box fontFamily='Segoe UI'>
+    <Box fontFamily='Segoe UI Custom'>
       <Routes>
         <Route path='/' element={<HomePage />}/>
         <Route path='/dashboard' element={<Dashboard />}/>
@@ -19,6 +19,7 @@ function App() {
         <Route path='/market' element={<BuyNFT />}/>
         <Route path='/earning' element={<Earning />}/>
         <Route path='/rewards' element={<Rewards />}/>
+        <Route path='/collection' element={<Collection />}/>
       </Routes>
     </Box>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router';
 import { system } from './components/ui/theme.js';
+import '@/style/style.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
